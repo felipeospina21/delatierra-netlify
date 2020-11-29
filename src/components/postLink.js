92787cf9bb2ddae0c620d4a9beee47346c51da9c
felipe ospina calle <felipeospina21@hotmail.com>
2020-11-29 18:14:32 -0500

    cms yml modified

--- src/components/postLink.js
+++ src/components/postLink.js
@@ -1,9 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
+import Image from './image'
+
+
+import "./postLink.scss"
 
 const PostLink = ({ post }) => (
   <div>
-    <Link to={'/' + post.frontmatter.slug}>
+    <Image src={post.frontmatter.image}/>
+    <Link to={"/" + post.frontmatter.slug}>
       {post.frontmatter.title} ({post.frontmatter.date})
     </Link>
   </div>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -19,7 +19,7 @@ const BlogPageTemplate = ({
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
-      <h1>Blog Posts</h1>
+      <h1>Recetas y tips</h1>
       {edges.map(edge => (
         <PostLink key={edge.node.id} post={edge.node} />
       ))}
